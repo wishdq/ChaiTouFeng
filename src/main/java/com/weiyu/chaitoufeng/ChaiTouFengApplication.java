7e48e7bdaf1fd786f780b31a70b3ec9de6011822
wishdq <wish_dq@163.com>
2022-03-05 16:34:30 +0800

    init

---
+++ src/main/java/com/weiyu/chaitoufeng/ChaiTouFengApplication.java
@@ -0,0 +1,13 @@
+package com.weiyu.chaitoufeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaiTouFengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaiTouFengApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weiyu/chaitoufeng/ChaiTouFengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weiyu.chaitoufeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaiTouFengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
